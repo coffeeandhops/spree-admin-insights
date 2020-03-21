c4e453cc5dac263f19f37f5160287d39b473aef9
Jason Kroll <jason@coffeeandhops.com>
2020-03-21 16:47:07 +1000

    module install

---
+++ lib/generators/spree_admin_insights/install/install_backend_generator.rb
@@ -0,0 +1,35 @@
+module SpreeAdminInsights
+  module Generators
+    module Install
+      class InstallBackendGenerator < Rails::Generators::Base
+        source_root(File.expand_path(File.dirname(__FILE__)))
+        class_option :auto_run_migrations, :type => :boolean, :default => false
+
+        def copy_initializer
+          copy_file 'spree_admin_insights.rb', 'config/initializers/spree_admin_insights.rb'
+        end
+
+        def add_javascripts
+          append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_admin_insights\n"
+        end
+
+        def add_stylesheets
+          inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_admin_insights\n", :before => /\*\//, :verbose => true
+        end
+
+        def add_migrations
+          run 'bundle exec rake railties:install:migrations FROM=spree_admin_insights'
+        end
+
+        def run_migrations
+          run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+          if run_migrations
+            run 'bundle exec rake db:migrate'
+          else
+            puts 'Skipping rake db:migrate, don\'t forget to run it!'
+          end
+        end
+      end
+    end
+  end
+end
